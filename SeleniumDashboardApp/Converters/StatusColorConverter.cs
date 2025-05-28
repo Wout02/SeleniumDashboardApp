786b0284cb2ea487366027488182ad55ccb084d1
WoutGeuze <55230010+Wout02@users.noreply.github.com>
2025-05-28 17:18:12 +0200

    Frontend aanpassingen

---
+++ SeleniumDashboardApp/Converters/StatusColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace SeleniumDashboardApp.Converters
+{
+    public class StatusColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString()?.ToLower() switch
+            {
+                "passed" => Colors.Green,
+                "failed" => Colors.Red,
+                _ => Colors.Gray
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SeleniumDashboardApp/Views/MainPage.xaml.cs
+++ SeleniumDashboardApp/Views/MainPage.xaml.cs
@@ -9,5 +9,16 @@ namespace SeleniumDashboardApp.Views
             InitializeComponent();
             BindingContext = viewModel;
         }
+
+        private void OnToggleSearchClicked(object sender, EventArgs e)
+        {
+            SearchEntry.IsVisible = !SearchEntry.IsVisible;
+        }
+
+        private void OnToggleFilterClicked(object sender, EventArgs e)
+        {
+            StatusPicker.IsVisible = !StatusPicker.IsVisible;
+            FilterButtons.IsVisible = !FilterButtons.IsVisible;
+        }
     }
 }
